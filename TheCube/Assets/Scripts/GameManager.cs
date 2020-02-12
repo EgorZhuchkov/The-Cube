9a27313a236c886b5d970df939f3b6941b8e2e57
Egor <egorzhuchkov2013@yandex.ru>
2020-02-13 01:26:48 +0300

    Add project

---
+++ TheCube/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameIsOver = false;
+    public float restartDelay = 1f;
+    public GameObject levelCompleteUI;
+    public PlayerMovement playerMovement;
+
+    public void CompleteLevel()
+    {
+        levelCompleteUI.SetActive(true);
+        playerMovement.enabled = false;
+    }
+
+    public void EndGame()
+    {
+        if(!gameIsOver)
+        {
+            gameIsOver = true;
+            Debug.Log("Game over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ TheCube/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 200f;
+    public float sidewaysForce = 500f;
+    string direction;
+
+    void Update()
+    {
+        if(Input.GetKey("d"))
+        {
+            direction = "right";
+        }
+        if(Input.GetKey("a"))
+        {
+            direction = "left";
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce);
+
+        if(direction == "right")
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (direction == "left")
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
